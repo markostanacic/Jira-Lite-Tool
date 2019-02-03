f142118e8d5b16eb2a5ae40ec01af90dcd35b7c6
markostanacic <marko.stanacic@gmail.com>
2019-02-03 23:06:18 +0100

    [no-issue] => added part of FE for project entity

--- jira-lite-client/src/App.js
+++ jira-lite-client/src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import { Provider } from "react-redux";
+import store from "./store";
+
 import Dashboard from "./components/Dashboard";
 import Header from "./components/layout/Header";
 import AddProject from "./components/project/AddProject";
@@ -10,13 +13,15 @@ import AddProject from "./components/project/AddProject";
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div>
-          <Header />
-          <Route exact path="/dashboard" component={Dashboard} />
-          <Route exact path="/addProject" component={AddProject} />
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div>
+            <Header />
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/addProject" component={AddProject} />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
